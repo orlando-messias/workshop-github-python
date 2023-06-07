49fd006c081397210277abc2f999cd77b87bee15
orlando-messias <programadorlando@gmail.com>
2023-06-06 21:50:18 -0300

    cria uma nova variavel para a idade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 name = input('Digite seu nome ')
 city = input('Digite a cidade ')
 gender = input('Digite o sexo ')
+age = input('Digite a idade ')
 
 print('Seguem os dados:')
 print(name, ' - ', city, ' - ', gender)
